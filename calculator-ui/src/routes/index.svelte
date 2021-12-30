3ab6083405d4f531db0ee9578d0a48f457fc9e83
blurrrb <17aakashsingh1999@gmail.com>
2021-12-30 10:33:30 +0000

    Add styling and layout

--- calculator-ui/src/routes/index.svelte
+++ calculator-ui/src/routes/index.svelte
@@ -1,28 +1,37 @@
-<div class="w-screen h-screen bg-black p-2">
-	<div class="h-1/4">
-		<!-- show input here -->
-		hello world
+<div class="w-screen h-screen bg-black normal-case">
+	<div class="h-2/5">
+		<div class="h-3/5 bg-green-400" />
+		<div class="h-1/5 px-8 flex items-center justify-center">
+			<span class="text-2xl font-extrabold text-green-800">=</span>
+			<span class="grow" />
+			<span class="text-2xl text-gray-500 float-right">999,999,999,999,999,999</span>
+		</div>
+		<div class="h-1/5 px-8">
+			<button class="h-full p-auto">
+				<img class="w-8" src="/icons/history.svg" alt="" srcset="" />
+			</button>
+			<button class="h-full p-auto float-right">
+				<img class="w-8" src="/icons/backspace.svg" alt="" srcset="" />
+			</button>
+		</div>
 	</div>
-	<div>
-		<!-- show result here -->
-	</div>
-	<div class="h-3/4 grid grid-cols-4 gap-2">
+	<div class="p-2 pt-0 h-3/5 grid grid-cols-4 gap-2">
 		<button class="text-2xl h-auto btn btn-error">C</button>
-		<button class="text-2xl h-auto btn">(</button>
-		<button class="text-2xl h-auto btn">)</button>
-		<button class="text-2xl h-auto btn">/</button>
+		<button class="text-2xl h-auto btn btn-warning">(</button>
+		<button class="text-2xl h-auto btn btn-warning">)</button>
+		<button class="text-2xl h-auto btn btn-warning">/</button>
 		<button class="text-2xl h-auto btn">7</button>
 		<button class="text-2xl h-auto btn">8</button>
 		<button class="text-2xl h-auto btn">9</button>
-		<button class="text-2xl h-auto btn">x</button>
+		<button class="text-2xl h-auto btn btn-warning normal-case">x</button>
 		<button class="text-2xl h-auto btn">4</button>
 		<button class="text-2xl h-auto btn">5</button>
 		<button class="text-2xl h-auto btn">6</button>
-		<button class="text-2xl h-auto btn">-</button>
+		<button class="text-2xl h-auto btn btn-warning">-</button>
 		<button class="text-2xl h-auto btn">1</button>
 		<button class="text-2xl h-auto btn">2</button>
 		<button class="text-2xl h-auto btn">3</button>
-		<button class="text-2xl h-auto btn">+</button>
+		<button class="text-2xl h-auto btn btn-warning">+</button>
 		<button class="text-2xl h-auto btn">+/-</button>
 		<button class="text-2xl h-auto btn">0</button>
 		<button class="text-2xl h-auto btn">.</button>
