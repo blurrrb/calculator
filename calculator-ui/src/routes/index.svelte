774e11aef2d7a5857edfe835d849890744b74fb9
blurrrb <17aakashsingh1999@gmail.com>
2021-12-30 08:04:21 +0000

    Basic config and layout

--- calculator-ui/src/routes/index.svelte
+++ calculator-ui/src/routes/index.svelte
@@ -1,2 +1,31 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="w-screen h-screen bg-black p-2">
+	<div class="h-1/4">
+		<!-- show input here -->
+		hello world
+	</div>
+	<div>
+		<!-- show result here -->
+	</div>
+	<div class="h-3/4 grid grid-cols-4 gap-2">
+		<button class="text-2xl h-auto btn btn-error">C</button>
+		<button class="text-2xl h-auto btn">(</button>
+		<button class="text-2xl h-auto btn">)</button>
+		<button class="text-2xl h-auto btn">/</button>
+		<button class="text-2xl h-auto btn">7</button>
+		<button class="text-2xl h-auto btn">8</button>
+		<button class="text-2xl h-auto btn">9</button>
+		<button class="text-2xl h-auto btn">x</button>
+		<button class="text-2xl h-auto btn">4</button>
+		<button class="text-2xl h-auto btn">5</button>
+		<button class="text-2xl h-auto btn">6</button>
+		<button class="text-2xl h-auto btn">-</button>
+		<button class="text-2xl h-auto btn">1</button>
+		<button class="text-2xl h-auto btn">2</button>
+		<button class="text-2xl h-auto btn">3</button>
+		<button class="text-2xl h-auto btn">+</button>
+		<button class="text-2xl h-auto btn">+/-</button>
+		<button class="text-2xl h-auto btn">0</button>
+		<button class="text-2xl h-auto btn">.</button>
+		<button class="text-2xl h-auto btn btn-success">=</button>
+	</div>
+</div>
